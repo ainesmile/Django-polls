d3b7146013e60f03ae80fdb85a84d5b93e11f293
ainesmile <ainesmile@gmail.com>
2017-03-13 16:32:47 +1300

    add views `results` and `vote`

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world!")
 
+def detail(request, question_id):
+    return HttpResponse("Detail %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("voting on question %s" % question_id)
